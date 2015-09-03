b3407455c9da083d9e2c3054bba72c3ebc003c28
nemanjamandic <nemanjans84@gmail.com>
2015-09-03 00:11:28 +0000

    The End

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,7 +5,7 @@ class MoviesController < ApplicationController
   # GET /movies
   # GET /movies.json
   def index
-    @movies = Movie.all.order(:cached_votes_score => :desc)
+    @movies = Movie.all.order(:cached_weighted_score => :desc)
   end
 
   # GET /movies/1
